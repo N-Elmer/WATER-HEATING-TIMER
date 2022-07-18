05d8483995ed8b323f0b1d6f5a328e776439c10d
Elmander Elmer <elmerelmander@gmail.com>
2022-07-18 03:08:27 +0100

    SOURCE FILES UPLOADED
    Launch files by running the main.py file using a CLI.

---
+++ model.py
@@ -0,0 +1,16 @@
+import sys
+import time
+import random
+import datetime
+import winsound
+from prettytable import ALL
+from datetime import datetime  
+from datetime import timedelta
+from prettytable import PrettyTable
+from header import Header_class
+from processing import Processing_class
+
+class Model_class():
+    def model_function():
+        Header_class.header_function()
+        Processing_class.processing_function()
